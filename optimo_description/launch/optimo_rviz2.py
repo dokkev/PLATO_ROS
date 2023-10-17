6bdc734840c45d6b95f0e9578fc72b2d8429a30b
dokkev <dhk6869@gmail.com>
2023-10-17 16:26:46 -0500

    rviz2 launch argumets for gui and sim_time added

--- optimo_description/launch/optimo_rviz2.py
+++ optimo_description/launch/optimo_rviz2.py
@@ -1,61 +1,76 @@
 import os
-from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument, LogInfo, SetEnvironmentVariable
-from launch.substitutions import LaunchConfiguration, Command
-from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
-import launch_ros.descriptions
-import launch.substitutions
-import launch_ros
-import launch_ros.substitutions
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition,UnlessCondition
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
+
+
+import xacro
+
 
 def generate_launch_description():
-    use_sim_time = LaunchConfiguration('use_sim_time', default='false')
 
-    optimo_description_package = launch_ros.substitutions.FindPackageShare(package='optimo_description').find('optimo_description')
+    # Arguments
+    use_sim_time = LaunchConfiguration('use_sim_time')
+    use_gui = LaunchConfiguration('use_gui')
 
-    rviz_config_file = get_package_share_directory('optimo_description') + '/rviz/optimo.rviz'
+    # getting the package path
+    pkg_name = 'optimo_description'
+    pkg_share= get_package_share_directory(pkg_name)
 
+    # URDF file path
+    urdf_path = 'urdf/optimo.urdf.xacro'
+
+    # RVIZ config file path
+    rviz_config_file = pkg_share + '/rviz/optimo.rviz'
+    
+    # extracting the robot deffinition from the xacro file
+    xacro_file = os.path.join(pkg_share, urdf_path)
+    robot_description_content = xacro.process_file(xacro_file).toxml()
+
+
+
+    # Run the nodes
     return LaunchDescription([
+
         DeclareLaunchArgument(
             'use_sim_time',
             default_value='false',
-            description='Use simulation (Gazebo) clock if true'),
-
-        
-        # Robot state publisher
-        Node(
-            package='robot_state_publisher',
-            executable='robot_state_publisher',
-            name='robot_state_publisher',
-            output='both',
-            parameters=[{
-                'use_sim_time': use_sim_time,
-                'robot_description': launch_ros.descriptions.ParameterValue( launch.substitutions.Command(['xacro ',os.path.join(optimo_description_package,'urdf/optimo.urdf.xacro')]), value_type=str)  
-            }]
-          
+            description='Use sim (Gazebo) clock when True'
         ),
 
-        # Joint state publisher
-        # Node(
-        #     package='joint_state_publisher',
-        #     executable='joint_state_publisher',
-        #     name='joint_state_publisher',
-        #     output='screen',
-        #     parameters=[{
-        #         'use_sim_time': use_sim_time,
-        #     }]
-        # ),
+        DeclareLaunchArgument(
+            'use_gui',
+            default_value='false',
+            description='Use joint_state_publisher_gui'
+        ),
 
         # Joint state publisher GUI
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
+            condition=IfCondition(use_gui),
+        ),
+
+        # Joint state publisher
+        Node(
+            package='joint_state_publisher',
+            executable='joint_state_publisher',
+            name='joint_state_publisher',
+            condition=UnlessCondition(use_gui),
+        ),
+        
+        # Robot state publisher
+        Node(
+            package='robot_state_publisher',
+            executable='robot_state_publisher',
             output='screen',
-            parameters=[{
-                'use_sim_time': use_sim_time,
-            }]
+            parameters=[{'robot_description': robot_description_content, 'use_sim_time': use_sim_time}]
         ),
         
         # Rviz2
@@ -63,8 +78,10 @@ def generate_launch_description():
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            output='screen',
-            arguments=['-d', rviz_config_file],
-            parameters=[{'use_sim_time': use_sim_time}]
+            arguments= ['-d', rviz_config_file]
         ),
+
+
+
+  
     ])

16cdeaf929cc8c06ece062266df375496daf460f
dokkev <dhk6869@gmail.com>
2023-10-22 12:57:26 -0500

    gazebo arm controller debugging

--- plato_gazebo/launch/optimo_gazebo.launch.py
+++ plato_gazebo/launch/optimo_gazebo.launch.py
@@ -5,16 +5,64 @@ from launch_ros.actions import Node
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import SetEnvironmentVariable
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command
 import xacro
 
 
 
 def generate_launch_description():
-    launch_gazebo = IncludeLaunchDescription(
-            PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py'])
-            
-        )
+
+   # Check if we're told to use sim time
+  
+
+    models_path = os.path.join(get_package_share_directory('plato_gazebo'), 'models')
+
+
+    ###### Nodes #####
+    spawn_entity_node = Node(package='gazebo_ros', executable='spawn_entity.py',
+                             arguments=['-topic', 'robot_description',
+                                        '-entity', 'optimo'],
+                             output='screen')
+    # spawning the joint broadcaster
+    spawn_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
+    )
+
+    spawn_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_trajectory_controller"],
+        output="screen",
+    )
+
+    ###### Include #####
+    rsp_launch = IncludeLaunchDescription(
+                PythonLaunchDescriptionSource([os.path.join(
+                    get_package_share_directory('optimo_description'),'launch','optimo_rsp.launch.py'
+                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
+    )
+
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={'world': os.path.join(get_package_share_directory('plato_gazebo'), 'worlds', 'optimo.world')}.items()
+    )
+
+
+
     
     return LaunchDescription([
-        launch_gazebo
+        # SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=model_path),
+        rsp_launch,
+        gazebo_launch,
+        spawn_broadcaster,
+        spawn_controller,
+        spawn_entity_node,
+        
+  
     ])

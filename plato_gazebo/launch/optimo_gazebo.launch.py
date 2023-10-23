783969de7d8a9c05b6c7ae1a26c070f4226acc10
dokkev <dhk6869@gmail.com>
2023-10-23 11:05:27 -0500

    add geomtry to base link

--- plato_gazebo/launch/optimo_gazebo.launch.py
+++ plato_gazebo/launch/optimo_gazebo.launch.py
@@ -20,6 +20,9 @@ def generate_launch_description():
 
     models_path = os.path.join(get_package_share_directory('plato_gazebo'), 'models')
 
+    # RVIZ config file path
+    rviz_config_file = get_package_share_directory('optimo_description') + '/rviz/optimo.rviz'
+
 
     ###### Nodes #####
     spawn_entity_node = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -41,6 +44,26 @@ def generate_launch_description():
         output="screen",
     )
 
+    pkg_name = 'chess_manipulator'  # the package name
+    pkg_share = get_package_share_directory(pkg_name)
+    urdf_path = 'description/manipulator.urdf.xacro'
+    xacro_file = os.path.join(pkg_share, urdf_path)
+    robot_description_content = xacro.process_file(xacro_file).toxml()
+    # robot state publisher node
+    panda_robot_state_publisher = Node(
+        package='robot_state_publisher',
+        executable='robot_state_publisher',
+        output='screen',
+        parameters=[{'robot_description': robot_description_content}]
+    )
+
+    node_rviz =  Node(
+        package='rviz2',
+        executable='rviz2',
+        name='rviz2',
+        arguments= ['-d', rviz_config_file]
+    )
+
     ###### Include #####
     rsp_launch = IncludeLaunchDescription(
                 PythonLaunchDescriptionSource([os.path.join(
@@ -48,21 +71,26 @@ def generate_launch_description():
                 )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
     )
 
+
+
+
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
         launch_arguments={'world': os.path.join(get_package_share_directory('plato_gazebo'), 'worlds', 'optimo.world')}.items()
     )
 
 
-
     
     return LaunchDescription([
         # SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=model_path),
         rsp_launch,
+        # panda_robot_state_publisher,
         gazebo_launch,
         spawn_broadcaster,
         spawn_controller,
         spawn_entity_node,
+        # node_rviz
+ 
         
   
     ])

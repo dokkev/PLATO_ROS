36879a7bcba2f1eaa94a431987859a321d3ab51c
dokkev <dhk6869@gmail.com>
2023-11-19 17:38:15 -0600

    model path added

--- plato_gazebo/launch/plato_manipulator_gazebo.launch.py
+++ plato_gazebo/launch/plato_manipulator_gazebo.launch.py
@@ -15,18 +15,18 @@ import xacro
 
 def generate_launch_description():
 
-   # Check if we're told to use sim time
-  
+    ##### Arguments #####
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    models_path = os.path.join(get_package_share_directory('plato_gazebo'), 'models')
-
-    # RVIZ config file path
-    rviz_config_file = get_package_share_directory('plato_description') + '/rviz/plato.rviz'
-
-
+    ##### Parameters #####
+    pkg_path = os.path.join(get_package_share_directory('plato_gazebo'))
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        model_path = os.environ['GAZEBO_MODEL_PATH'] + ':' + 'models'+ pkg_path
+    else:
+        model_path = 'models'+ pkg_path
+    world_path = os.path.join(pkg_path, 'worlds', 'plato.world')
     plato_xacro = os.path.join(get_package_share_directory('plato_moveit_config'), 'config', 'plato.urdf.xacro')
-    robot_description_content = xacro.process_file(plato_xacro).toxml()
-
+    robot_description_content = xacro.process_file(plato_xacro, mappings={"hardware_type": "gazebo"}).toxml()
 
     ###### Nodes #####
     spawn_entity_node = Node(package='gazebo_ros', executable='spawn_entity.py',
@@ -41,15 +41,13 @@ def generate_launch_description():
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
-        # output='screen',
-        parameters=[{'robot_description': robot_description_content}],
+        output='screen',
+        parameters=[{'use_sim_time': use_sim_time}, {'robot_description': robot_description_content}]
 
     )
 
-
     # spawning the joint broadcaster
     spawn_broadcaster = Node(
-
         package="controller_manager",
         executable="spawner",
         arguments=["joint_state_broadcaster"],
@@ -70,40 +68,28 @@ def generate_launch_description():
         output="screen",
     )
 
-  
-    node_rviz =  Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        arguments= ['-d', rviz_config_file]
-    )
+    
+
 
     ###### Include #####
-    rsp_launch = IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([os.path.join(
-                    get_package_share_directory('plato_description'),'launch','plato_manipulator_rsp.launch.py'
-                )]), launch_arguments={'use_sim_time': 'true', 'use_ros2_control': 'true'}.items()
-    )
-
 
     gazebo_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
-        launch_arguments={'world': os.path.join(get_package_share_directory('plato_gazebo'), 'worlds', 'optimo.world')}.items()
+        launch_arguments={'world': world_path}.items()
     )
 
    
-
-    
     return LaunchDescription([
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=model_path),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use sim time if true'),
         node_robot_state_publisher,
         gazebo_launch,
         spawn_broadcaster,
         spawn_optimo_controller,
         spawn_plato_controller,
-        spawn_entity_node,
-        # node_rviz
-    
- 
-        
+        spawn_entity_node,   
   
     ])

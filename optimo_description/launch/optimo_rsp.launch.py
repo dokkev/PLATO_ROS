592c938b708c8bf4c92f1c981d4ba3f5dc980a39
dokkev <dhk6869@gmail.com>
2023-11-19 15:10:51 -0600

    ros2_control arg deleted

--- optimo_description/launch/optimo_rsp.launch.py
+++ optimo_description/launch/optimo_rsp.launch.py
@@ -14,12 +14,11 @@ def generate_launch_description():
 
     # Check if we're told to use sim time
     use_sim_time = LaunchConfiguration('use_sim_time')
-    use_ros2_control = LaunchConfiguration('use_ros2_control')
 
     # Process the URDF file
     pkg_path = os.path.join(get_package_share_directory('optimo_description'))
     xacro_file = os.path.join(pkg_path,'urdf','optimo.urdf.xacro')
-    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
+    robot_description_config = Command(['xacro ', xacro_file, ' sim_mode:=', use_sim_time])
     
     # Create a robot_state_publisher node
     node_robot_state_publisher = Node(
@@ -36,10 +35,5 @@ def generate_launch_description():
             'use_sim_time',
             default_value='false',
             description='Use sim time if true'),
-        DeclareLaunchArgument(
-            'use_ros2_control',
-            default_value='true',
-            description='Use ros2_control if true'),
-
         node_robot_state_publisher
     ])

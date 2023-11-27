fc544957a870f94ebb9a200c72a75adc902ee02f
dokkev <dhk6869@gmail.com>
2023-11-26 19:35:53 -0600

    more yaml for mp added

--- plato_moveit_control/launch/plato_control.launch.py
+++ plato_moveit_control/launch/plato_control.launch.py
@@ -8,35 +8,57 @@ from launch_ros.actions import Node
 from launch.actions import DeclareLaunchArgument, ExecuteProcess
 from launch.substitutions import LaunchConfiguration
 from moveit_configs_utils import MoveItConfigsBuilder
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 
 def generate_launch_description():
     moveit_config = (
         MoveItConfigsBuilder(
-            robot_name="panda", package_name="moveit_resources_panda_moveit_config"
+            robot_name="plato", package_name="plato_moveit_config"
         )
-        .robot_description(file_path="config/panda.urdf.xacro")
-        .trajectory_execution(file_path="config/gripper_moveit_controllers.yaml")
+        .robot_description(file_path="config/plato.urdf.xacro")
+        .trajectory_execution(file_path="config/moveit_controllers.yaml")
         .moveit_cpp(
-            file_path=get_package_share_directory("python_examples")
-            + "/config/motion_planning_python.yaml"
+            file_path=get_package_share_directory("plato_moveit_control")
+            + "/config/motion_planning.yaml"
         )
         .to_moveit_configs()
     )
 
-    example_file = DeclareLaunchArgument(
-        "example_file",
-        default_value="pose_goal",
-        description="Python API tutorial file name",
+    # example_file = DeclareLaunchArgument(
+    #     "example_file",
+    #     default_value="control.py",
+    #     description="Python API tutorial file name",
+    # )
+
+    # moveit_py_node = Node(
+    #     name="moveit_py",
+    #     package="plato_moveit_control",
+    #     executable=LaunchConfiguration("example_file"),
+    #     output="both",
+    #     parameters=[moveit_config.to_dict()],
+    # )
+
+    rviz_config_file = os.path.join(
+        get_package_share_directory("plato_moveit_config"),
+        "config",
+        "moveit.rviz",
     )
 
-    moveit_py_node = Node(
-        name="moveit_py",
-        package="python_examples",
-        executable=LaunchConfiguration("example_file"),
-        output="both",
-        parameters=[moveit_config.to_dict()],
+    rviz_node = Node(
+        package="rviz2",
+        executable="rviz2",
+        name="rviz2",
+        output="log",
+        arguments=["-d", rviz_config_file],
+        parameters=[
+            moveit_config.robot_description,
+            moveit_config.robot_description_semantic,
+        ],
     )
+
+
 
     robot_state_publisher = Node(
         package="robot_state_publisher",
@@ -47,7 +69,7 @@ def generate_launch_description():
     )
 
     ros2_controllers_path = os.path.join(
-        get_package_share_directory("moveit_resources_panda_moveit_config"),
+        get_package_share_directory("plato_moveit_config"),
         "config",
         "ros2_controllers.yaml",
     )
@@ -57,6 +79,10 @@ def generate_launch_description():
         parameters=[moveit_config.robot_description, ros2_controllers_path],
         output="log",
     )
+
+    move_group = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('plato_moveit_config'), 'launch'), '/move_group.launch.py']))
+    
 
     load_controllers = []
     for controller in [
@@ -72,12 +98,16 @@ def generate_launch_description():
             )
         ]
 
+
+
     return LaunchDescription(
         [
-            example_file,
-            moveit_py_node,
+            # example_file,
+            # moveit_py_node,
             robot_state_publisher,
             ros2_control_node,
+            move_group,
+            rviz_node,
         ]
         + load_controllers
     )

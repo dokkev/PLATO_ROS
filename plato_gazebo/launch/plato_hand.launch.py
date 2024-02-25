e18f9976f2bc01940006b9926b019a522f5f8342
Dong Ho Kang <dongho@utexas.edu>
2024-02-25 16:43:56 -0600

    gazebo works plato hand

--- plato_gazebo/launch/plato_hand.launch.py
+++ plato_gazebo/launch/plato_hand.launch.py
@@ -1,98 +1,85 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, ExecuteProcess, RegisterEventHandler
-from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.event_handlers import OnProcessExit
-
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription, Shutdown, GroupAction
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.actions import SetEnvironmentVariable
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution, FindExecutable
+from launch_ros.substitutions import FindPackageShare
 import xacro
 
-
 def generate_launch_description():
-    robot_name = 'plato_description'
-    world_file_name = 'empty.world'
 
-    # Parsing URDF.XACRO
-    xacro_file = os.path.join(get_package_share_directory(
-        robot_name), 'urdf', 'plato_hand.urdf.xacro')    
-    xml_qarm = xacro.process_file(xacro_file).toxml()#.replace('"', '\\"')
+    ##### Arguments #####
+    use_sim_time = LaunchConfiguration('use_sim_time')
 
-    # Configuring RVIZ
-    rviz = os.path.join(get_package_share_directory(
-        robot_name), 'rviz', 'plato.rviz')
+    ##### Parameters #####
+    pkg_path = os.path.join(get_package_share_directory('plato_gazebo'))
+    models_path = os.path.join(get_package_share_directory('plato_gazebo'), 'models')
+    world_path = os.path.join(pkg_path, 'worlds', 'plato.world')
+    plato_xacro = os.path.join(get_package_share_directory('plato_description'), 'urdf', 'plato_hand.urdf.xacro')
+    robot_description_content = xacro.process_file(plato_xacro, mappings={"gazebo_classic": "true"}).toxml()
 
-    # Configuring RVIZ Node
-    rviz_node = Node(
-        package='rviz2',
-        executable='rviz2',
-        name='rviz2',
-        output='screen',
-        arguments=['-d', rviz, '--title', 'QArm RVIZ'],
-    )
+    ##### Gazebo Path #####
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        model_path = os.environ['GAZEBO_MODEL_PATH'] + ':' + models_path
+    else:
+        model_path = models_path
 
-    # Robot State Publisher Node
+    ###### Nodes #####
+    spawn_entity_node = Node(package='gazebo_ros', executable='spawn_entity.py',
+                             arguments=['-topic', 'robot_description',
+                                        '-entity', 'plato_manipulator',
+                                        '-x', '0.1',
+                                        '-y', '0.1',
+                                        '-z', '0.01'
+                                        
+                                        ],
+                             output='screen')
+    
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
-        parameters=[{'robot_description': xml_qarm,
-        'use_sim_time': True}]
+        parameters=[{'use_sim_time': use_sim_time}, {'robot_description': robot_description_content}]
     )
 
-    # Configuring Gazebo
-    gazebo= ExecuteProcess(
-        cmd=['gazebo', '-u', os.path.join(get_package_share_directory("plato_gazebo"), 'worlds', 'qarm_world_1.world'), "--verbose",
-             "-s", "libgazebo_ros_factory.so",
-             "-s", "libgazebo_ros_init.so", "--ros-args", "--params-file", os.path.join(get_package_share_directory("plato_gazebo"), 'config', 'params_init.yaml'), 
-             ],
-        output='screen'
-    )
-
-    # Spawning Robot Arm
-    spawn_entity = Node(package='gazebo_ros', executable='spawn_entity.py',
-                    arguments=['-topic', 'robot_description',
-                                '-entity', 'qarm_v1'],
-                    output='screen')
-
-    # Joint State Broadcaster
-    joint_state_broadcaster = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'joint_state_broadcaster'],
-        output='screen'
-    )
-
-    # Joint Trajectory Controller
-    joint_arm_position_controller = ExecuteProcess(
-        cmd=['ros2', 'control', 'load_controller', '--set-state', 'active',
-             'plato_joint_controller'],
-        output='screen'
+    # spawning the joint broadcaster
+    spawn_broadcaster = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["joint_state_broadcaster"],
+        output="screen",
     )
 
 
 
-    # Run the node
+    spawn_plato_controller = Node(
+        package="controller_manager",
+        executable="spawner",
+        arguments=["plato_joint_controller"],
+        output="screen",
+    )
+
+    ###### Include #####
+    gazebo_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([os.path.join(get_package_share_directory('gazebo_ros'), 'launch'), '/gazebo.launch.py']),
+        launch_arguments={'world': world_path}.items()
+    )
+
     return LaunchDescription([
-        gazebo,
+        SetEnvironmentVariable(name='GAZEBO_MODEL_PATH', value=model_path),
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='true',
+            description='Use sim time if true'),
         node_robot_state_publisher,
-        spawn_entity,
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=spawn_entity,
-                on_exit=[joint_state_broadcaster], 
-            ) # joint_state_broadcaster starts after entity is spawned
-        ),
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster,
-                on_exit=[joint_arm_position_controller],
-            ) # controllers only after joint_state_broadcaster
-        ),
-
-        RegisterEventHandler(
-            event_handler=OnProcessExit(
-                target_action=joint_state_broadcaster,
-                on_exit=[rviz_node],
-            ) # rviz ohnly after joint_state_broadcaster
-        ),
+        gazebo_launch,
+        spawn_broadcaster,
+        spawn_plato_controller,
+        spawn_entity_node,   
     ])

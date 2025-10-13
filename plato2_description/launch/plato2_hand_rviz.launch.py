10259cd2ff2be5a27f6ae2ee8ffe5b8b2bd3c672
lklinus2609 <lklinus0926@gmail.com>
2025-10-13 14:45:55 -0500

    revert back to p2 urdf

--- plato2_description/launch/plato2_hand_rviz.launch.py
+++ plato2_description/launch/plato2_hand_rviz.launch.py
@@ -1,55 +1,87 @@
 import os
 from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
-from launch.conditions import IfCondition, UnlessCondition
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
+from launch.actions import IncludeLaunchDescription
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.conditions import IfCondition,UnlessCondition
+from launch.actions import DeclareLaunchArgument, LogInfo
+from launch.substitutions import LaunchConfiguration, Command, PathJoinSubstitution
 import xacro
 
+
 def generate_launch_description():
+
+    # Arguments
     use_sim_time = LaunchConfiguration('use_sim_time')
     use_gui = LaunchConfiguration('use_gui')
 
-    pkg_share = get_package_share_directory('plato2_description')
-    xacro_file = os.path.join(pkg_share, 'urdf', 'plato2.urdf.xacro')
-    rviz_config = os.path.join(pkg_share, 'rviz', 'plato_description.rviz')
+    # getting the package path
+    pkg_name = 'plato2_description'
+    pkg_share= get_package_share_directory(pkg_name)
 
-    robot_description = xacro.process_file(xacro_file).toxml()
+    # URDF file path
+    urdf_path = 'urdf/plato2.urdf.xacro'
 
+    # RVIZ config file path
+    rviz_config_file = pkg_share + '/rviz/plato_description.rviz'
+    
+    # extracting the robot deffinition from the xacro file
+    xacro_file = os.path.join(pkg_share, urdf_path)
+    robot_description_content = xacro.process_file(xacro_file).toxml()
+
+
+
+    # Run the nodes
     return LaunchDescription([
-        DeclareLaunchArgument('use_sim_time', default_value='false'),
-        DeclareLaunchArgument('use_gui', default_value='true'),
 
-        # GUI joint state publisher
+        ##################### Arguments #####################
+        
+        DeclareLaunchArgument(
+            'use_sim_time',
+            default_value='false',
+            description='Use sim (Gazebo) clock when True'
+        ),
+
+        DeclareLaunchArgument(
+            'use_gui',
+            default_value='true',
+            description='Use joint_state_publisher_gui'
+        ),
+
+
+        ##################### Nodes #####################
+
+        # Joint state publisher GUI
         Node(
             package='joint_state_publisher_gui',
             executable='joint_state_publisher_gui',
             name='joint_state_publisher_gui',
             condition=IfCondition(use_gui),
         ),
-        # Non-GUI fallback
+
+        # Joint state publisher
         Node(
             package='joint_state_publisher',
             executable='joint_state_publisher',
             name='joint_state_publisher',
             condition=UnlessCondition(use_gui),
         ),
-
+        
+        # Robot state publisher
         Node(
             package='robot_state_publisher',
             executable='robot_state_publisher',
             output='screen',
-            parameters=[{
-                'robot_description': robot_description,
-                'use_sim_time': use_sim_time
-            }]
+            parameters=[{'robot_description': robot_description_content, 'use_sim_time': use_sim_time}]
         ),
-
+        
+        # Rviz2
         Node(
             package='rviz2',
             executable='rviz2',
             name='rviz2',
-            arguments=['-d', rviz_config],
+            arguments= ['-d', rviz_config_file]
         ),
+
     ])

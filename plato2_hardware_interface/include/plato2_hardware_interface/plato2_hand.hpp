3350bc632b336307db07beea83d9d747acaf49d3
dhk6869@gmail.com <lklinus0926@gmail.com>
2025-10-23 18:30:44 -0500

    plato2_hand optimization

--- plato2_hardware_interface/include/plato2_hardware_interface/plato2_hand.hpp
+++ plato2_hardware_interface/include/plato2_hardware_interface/plato2_hand.hpp
@@ -1,169 +1,100 @@
 #ifndef PLATO_HARDWARE_INTERFACE__PLATO2_HAND_HPP_
 #define PLATO_HARDWARE_INTERFACE__PLATO2_HAND_HPP_
 
-
 #include "plato2_hardware_interface/actuator.hpp"
 #include "plato2_hardware_interface/ft_sensor.hpp"
-#include "plato2_hardware_interface/karnopp_compensator.hpp"
 
 #include <geometry_msgs/msg/wrench.hpp>
 
+namespace plato2_hand {
 
-namespace plato2_hand{
-
-
-struct States{
-    std::vector<float> position;
-    std::vector<float> velocity;
-    std::vector<float> torque;
-
-    std::vector<float> force_raw;
-    std::vector<float> torque_raw;
-    std::vector<float> force_filtered;
-    std::vector<float> torque_filtered;
-};
-
-
-
-enum class ControlMode {
-    OFF,      // Sends Stop Control Cmd to the motors. Joint states are not updated
-    IDLE,     // Send Zero Torque Command to the motors. Joint states are updated
-    POSITION, // Send Position Command to the motors. Joint states are updated
-    VELOCITY, // Send Velocity Command to the motors. Joint states are updated
-    TORQUE,   // Send Torque Command to the motors. Joint states are updated
-};
-
-class Hand{
+class Hand {
 public:
-
-    /// @brief Constructor for the Hand. It initializes the PMOTOR Interface and the Actuators 
+    /// @brief Constructor - initializes PCAN interface and actuators
     Hand(pcan_interface::PCANInterface& pcan_interface);
 
-    /// @brief  Destructor for the Hand. It stops the motion control
+    /// @brief Destructor - disables motors safely
     ~Hand();
 
-    /// @brief Initialize the actuator and sensors communicating via CAN bus. Called in the constructor
-    void init_can_hardware();
-
-    /// @brief Enable the motors
+    /// @brief Enable all motors
     void enable();
 
-    /// @brief Disable the motors and set the motors to stop position when the motor is in motion
+    /// @brief Disable all motors
     void disable();
 
-    /// @brief Stop the motion control
-    void stop();
+    /// @brief Set current position as zero reference for all actuators
+    void set_current_position_as_zero();
 
-    /// @brief Off Command fuction
-    void set_off_command_();
-
-    /// @brief  Send IMPEDANCE command using PD controller + feedforward torque
-    /// @param joint_position_command
-    /// @param joint_velocity_command
-    /// @param joint_stiffness_command
-    /// @param joint_damping_command
-    /// @param joint_torque_command
+    /// @brief Send impedance command to all actuators
+    /// @param joint_position_command Target joint positions
+    /// @param joint_velocity_command Target joint velocities
+    /// @param joint_stiffness_command Stiffness gains (Kp)
+    /// @param joint_damping_command Damping gains (Kd)
+    /// @param joint_torque_command Feedforward torques
     void set_impedance_command(const std::vector<double> &joint_position_command,
                                const std::vector<double> &joint_velocity_command,
                                const std::vector<double> &joint_stiffness_command,
                                const std::vector<double> &joint_damping_command,
                                const std::vector<double> &joint_torque_command);
 
-    /// @brief Update the joint states of from the actuators
-    void update_joint_states(std::vector<double> &joint_position_states, std::vector<double> &joint_velocity_states, std::vector<double> &joint_effort_states);
+    /// @brief Update joint states from actuator feedback
+    void update_joint_states(std::vector<double> &joint_position_states, 
+                             std::vector<double> &joint_velocity_states, 
+                             std::vector<double> &joint_effort_states);
 
-    /// @brief Update the force-torque sensor states
+    /// @brief Update force-torque sensor states
     void update_ft_sensor_states(std::vector<geometry_msgs::msg::Wrench> &ft_sensor_states);
 
-    /// @brief  
-    void print_motor_positions();
-
-    void get_actuators_temperature();
-
-    void set_current_position_as_zero();
-
-   
-
-    /// @brief Return function for num of actuators and sensors
+    /// @brief Get number of actuators
     size_t get_num_actuators() const { return num_actuators_; }
+
+    /// @brief Get number of force-torque sensors
     size_t get_num_ft_sensors() const { return num_ft_sensors_; }
 
-
 private:
-    /// @brief PCAN Interface to communicate with motors
+    /// @brief Initialize CAN hardware (actuators and sensors)
+    void init_can_hardware();
+
+    /// @brief Print actuator and sensor initialization info
+    void print_actuator_info_();
+
+    // Hardware interfaces
     pcan_interface::PCANInterface &pcan_interface_;
 
-    /// @brief const actuator size for loop iteration
+    // Constants
     const size_t num_actuators_;
-
     const size_t num_ft_sensors_;
 
-
-    /// @brief Actuator Vector
+    // Hardware components
     std::vector<actuator::Actuator> actuators_;
     std::vector<sensor::FTSensor> ft_sensors_;
 
-    // MOTOR1 // JOINT1: Thumb CMC Roll (const)
-    // MOTOR2 // JOINT2: Thumb CMC Yaw (const)
-    // MOTOR3 // JOINT3: Thumb MCP (const)
-    // MOTOR4 // JOINT4: Thumb IP  
-    // MOTOR5 // JOINT5: Index MCP (const)
-    // MOTOR6 // JOINT6: Index PIP 
-    // MOTOR7 // JOINT7: Middle MCP (const)  
-    // MOTOR8 // JOINT8: Middle PIP
-
-    //TODO Figure out if we need this
-    /// @brief Reduction Ratios
-    std::vector<float> pos_ratios_;
-    std::vector<float> vel_ratios_;
-    std::vector<float> trq_ratios_;
-
-    std::vector<double> actuators_temperature_;
-
-
-    /// @brief Predefined Actuator Vector Map to corresponding RX CAN ID
+    // CAN message routing maps
     std::unordered_map<uint32_t, actuator::Actuator*> actuator_rx_id_map_;
     std::unordered_map<uint32_t, sensor::FTSensor*> ft_sensor_rx_id_map_;
 
-    /// @brief Internal Counter
-    uint32_t counter_ = 0;
-
-
-    /// @brief Karnopp Friction Compensator
-    std::vector<KarnoppCompensator> friction_compensators_;
-
-
-    ///////////////////////////////////////////////// PRIVATE FUNCTIONS //////////////////////////////////////////////
-
-    /// @brief Print the Actuator Information
-    void print_actuator_info_();
-
-    /// @brief read RX CAN messages from the Bus using the PCAN Interface and sort the messages to the corresponding actuators
-    void sort_can_rx_id_(const TPCANMsg &msg);
-
-    ///////////////////////////////////////////////// CONFIGURATIONS //////////////////////////////////////////////
-
-    /// @brief Predefined Actuator Configurations
+    // Hardware configurations
+    /// @brief Actuator configurations (Motor 1-8)
     std::vector<actuator::Config> actuator_configs_ = {
-        actuator::Config{MotorTxID::MOTOR1, MotorRxID::MOTOR1, MotorOffset::MOTOR1,  MotorDirection::MOTOR1, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::THUMB_ROLL_MAX, MotorPositionLimit::THUMB_ROLL_MIN},
-        actuator::Config{MotorTxID::MOTOR2, MotorRxID::MOTOR2, MotorOffset::MOTOR2,  MotorDirection::MOTOR2, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::THUMB_YAW_MAX, MotorPositionLimit::THUMB_YAW_MIN},
-        actuator::Config{MotorTxID::MOTOR3, MotorRxID::MOTOR3, MotorOffset::MOTOR3,  MotorDirection::MOTOR3, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::MCP_MAX, MotorPositionLimit::MCP_MIN},
-        actuator::Config{MotorTxID::MOTOR4, MotorRxID::MOTOR4, MotorOffset::MOTOR4,  MotorDirection::MOTOR4, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::PIP_MAX, MotorPositionLimit::PIP_MIN},
-        actuator::Config{MotorTxID::MOTOR5, MotorRxID::MOTOR5, MotorOffset::MOTOR5,  MotorDirection::MOTOR5, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::MCP_MAX, MotorPositionLimit::MCP_MIN},
-        actuator::Config{MotorTxID::MOTOR6, MotorRxID::MOTOR6, MotorOffset::MOTOR6,  MotorDirection::MOTOR6, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::PIP_MAX, MotorPositionLimit::PIP_MIN},
-        actuator::Config{MotorTxID::MOTOR7, MotorRxID::MOTOR7, MotorOffset::MOTOR7,  MotorDirection::MOTOR7, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::MCP_MAX, MotorPositionLimit::MCP_MIN},
-        actuator::Config{MotorTxID::MOTOR8, MotorRxID::MOTOR8, MotorOffset::MOTOR8,  MotorDirection::MOTOR8, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::PIP_MAX, MotorPositionLimit::PIP_MIN}
+        {MotorTxID::MOTOR1, MotorRxID::MOTOR1, MotorOffset::MOTOR1, MotorDirection::MOTOR1, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::THUMB_ROLL_MAX, MotorPositionLimit::THUMB_ROLL_MIN},
+        {MotorTxID::MOTOR2, MotorRxID::MOTOR2, MotorOffset::MOTOR2, MotorDirection::MOTOR2, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::THUMB_YAW_MAX, MotorPositionLimit::THUMB_YAW_MIN},
+        {MotorTxID::MOTOR3, MotorRxID::MOTOR3, MotorOffset::MOTOR3, MotorDirection::MOTOR3, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::MCP_MAX, MotorPositionLimit::MCP_MIN},
+        {MotorTxID::MOTOR4, MotorRxID::MOTOR4, MotorOffset::MOTOR4, MotorDirection::MOTOR4, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::PIP_MAX, MotorPositionLimit::PIP_MIN},
+        {MotorTxID::MOTOR5, MotorRxID::MOTOR5, MotorOffset::MOTOR5, MotorDirection::MOTOR5, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::MCP_MAX, MotorPositionLimit::MCP_MIN},
+        {MotorTxID::MOTOR6, MotorRxID::MOTOR6, MotorOffset::MOTOR6, MotorDirection::MOTOR6, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::PIP_MAX, MotorPositionLimit::PIP_MIN},
+        {MotorTxID::MOTOR7, MotorRxID::MOTOR7, MotorOffset::MOTOR7, MotorDirection::MOTOR7, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::MCP_MAX, MotorPositionLimit::MCP_MIN},
+        {MotorTxID::MOTOR8, MotorRxID::MOTOR8, MotorOffset::MOTOR8, MotorDirection::MOTOR8, GIM3505::TORQUE_CONSTANT, GIM3505::GEAR_RATIO, MotorPositionLimit::PIP_MAX, MotorPositionLimit::PIP_MIN}
     };
 
+    /// @brief Force-torque sensor configurations
     std::vector<sensor::Config> ft_sensor_configs_ = {
-        // sensor::Config{FTSensorID::THUMB_FORCE, FTSensorID::THUMB_TORQUE},
-        // sensor::Config{FTSensorID::INDEX_FORCE, FTSensorID::INDEX_TORQUE},
-        // sensor::Config{FTSensorID::MIDDLE_FORCE, FTSensorID::MIDDLE_TORQUE}
+        // Uncomment when FT sensors are used:
+        // {FTSensorID::THUMB_FORCE, FTSensorID::THUMB_TORQUE},
+        // {FTSensorID::INDEX_FORCE, FTSensorID::INDEX_TORQUE},
+        // {FTSensorID::MIDDLE_FORCE, FTSensorID::MIDDLE_TORQUE}
     };
-
 };
-
 
 } // namespace plato2_hand
 
-#endif // PLATO_HARDWARE_INTERFACE__PMOTOR_INTERFACE_HPP_
+#endif // PLATO_HARDWARE_INTERFACE__PLATO2_HAND_HPP_
--- plato2_hardware_interface/src/plato2_hand.cpp
+++ plato2_hardware_interface/src/plato2_hand.cpp
@@ -1,41 +1,26 @@
 #include <plato2_hardware_interface/plato2_hand.hpp>
 
-
-namespace plato2_hand{
+namespace plato2_hand {
 
 Hand::Hand(pcan_interface::PCANInterface &pcan_interface) 
-    :          pcan_interface_(pcan_interface),
-               num_actuators_(8),
-               num_ft_sensors_(3){
+    : pcan_interface_(pcan_interface),
+      num_actuators_(8),
+      num_ft_sensors_(3) {
 
-    // set actuator temperature vector size
-    actuators_temperature_.resize(num_actuators_);
-
-
-    // Initialize the actuators
     init_can_hardware();
-
-    // Print the actuator info
     print_actuator_info_();
-
-    // enable the motors
-    // enable();
-
+    
     // Empty the CAN buffer
     pcan_interface_.receive_message();
-
 }
 
 ////////////////////////////////////////////////////////////////////////
 
-Hand::~Hand(){
-    // Stop the motion control
-    std::cout << "Stopping the motion control..." << std::endl;
-    for (size_t i=0; i < num_actuators_; ++i){
-        actuators_[i].disable_motor();
+Hand::~Hand() {
+    std::cout << "Disabling motors..." << std::endl;
+    for (auto &actuator : actuators_) {
+        actuator.disable_motor();
     }
-    std::cout << "Disabling the motors..." << std::endl;
-    
 }
 
 ////////////////////////////////////////////////////////////////////////
@@ -88,30 +73,27 @@ void Hand::init_can_hardware() {
 
 ////////////////////////////////////////////////////////////////////////
 
-void Hand::enable(){
-    for (auto &actuator : actuators_){
+void Hand::enable() {
+    for (auto &actuator : actuators_) {
         actuator.enable_motor();
- 
     }
 }
 
 ////////////////////////////////////////////////////////////////////////
 
-void Hand::disable(){
-    // GIM3505
-
+void Hand::disable() {
+    for (auto &actuator : actuators_) {
+        actuator.disable_motor();
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////
 
-void Hand::stop(){
-    // Don't use this function.
-    // GIM3505
-     for (size_t i=0; i < num_actuators_; ++i){
-        actuators_[i].stop_control();
+void Hand::set_current_position_as_zero() {
+    for (auto &actuator : actuators_) {
+        actuator.set_current_position_as_zero();
     }
 }
-
 
 ////////////////////////////////////////////////////////////////////////
 
@@ -121,27 +103,23 @@ void Hand::set_impedance_command(const std::vector<double> &joint_position_comma
                                  const std::vector<double> &joint_damping_command,
                                  const std::vector<double> &joint_torque_command) {
 
-    // The controller provides a RELATIVE angle for the PIP joint (relative to the MCP link).
-    // The hardware needs an ABSOLUTE angle for the PIP motor (relative to the palm).
-    // The conversion is: pip_motor_absolute = pip_joint_relative + mcp_joint_absolute_STATE.
-    // This compensation is applied directly within the loop for the affected joints.
+    // Apply kinematic compensation for PIP joints (3, 5, 7).
+    // The URDF models PIP joints relative to their MCP link,
+    // but the hardware motors are grounded to the palm.
+    // Conversion: pip_motor_absolute = pip_joint_relative + mcp_joint_state
 
     for (size_t i = 0; i < num_actuators_; ++i) {
         float pos_cmd = joint_position_command[i];
 
-        // Apply compensation for PIP joints by adding the measured state of the parent MCP joint.
-        if (i == 3) { // Index finger PIP (parent MCP is joint 2)
-            const auto& mcp_state = actuators_[2].get_states();
-            pos_cmd += mcp_state.position;
-        } else if (i == 5) { // Middle finger PIP (parent MCP is joint 4)
-            const auto& mcp_state = actuators_[4].get_states();
-            pos_cmd += mcp_state.position;
-        } else if (i == 7) { // Ring/Pinky finger PIP (parent MCP is joint 6)
-            const auto& mcp_state = actuators_[6].get_states();
-            pos_cmd += mcp_state.position;
+        // Compensate PIP joints by adding the current MCP state
+        if (i == 3) { // Index finger PIP (parent is MCP joint 2)
+            pos_cmd += actuators_[2].get_states().position;
+        } else if (i == 5) { // Middle finger PIP (parent is MCP joint 4)
+            pos_cmd += actuators_[4].get_states().position;
+        } else if (i == 7) { // Ring/Pinky finger PIP (parent is MCP joint 6)
+            pos_cmd += actuators_[6].get_states().position;
         }
 
-        // Send the final command (compensated or direct) to the actuator.
         actuators_[i].set_joint_impedance(
             pos_cmd, 
             joint_velocity_command[i], 
@@ -154,57 +132,37 @@ void Hand::set_impedance_command(const std::vector<double> &joint_position_comma
 
 ////////////////////////////////////////////////////////////////////////
 
-
-void Hand::set_current_position_as_zero(){
-    for (size_t i = 0; i < num_actuators_; ++i){
-        actuators_[i].set_current_position_as_zero();
-    }
-}
-
-
-////////////////////////////////////////////////////////////////////////
-
-void Hand::update_joint_states(std::vector<double>&joint_position_states, std::vector<double>&joint_velocity_states, std::vector<double> &joint_effort_states){
-    // recevie the message from the CAN bus every loop
+void Hand::update_joint_states(std::vector<double> &joint_position_states, 
+                               std::vector<double> &joint_velocity_states, 
+                               std::vector<double> &joint_effort_states) {
+    // Receive CAN messages
     pcan_interface_.receive_message();
 
-    for (size_t i = 0; i < num_actuators_; ++i){
-        joint_position_states[i] = static_cast<double>(actuators_[i].get_states().position);
-        joint_velocity_states[i] = static_cast<double>(actuators_[i].get_states().velocity);
-        joint_effort_states[i] = static_cast<double>(actuators_[i].get_states().torque);
+    // Read raw motor states
+    for (size_t i = 0; i < num_actuators_; ++i) {
+        const auto& states = actuators_[i].get_states();
+        joint_position_states[i] = static_cast<double>(states.position);
+        joint_velocity_states[i] = static_cast<double>(states.velocity);
+        joint_effort_states[i] = static_cast<double>(states.torque);
     }
 
-    // Compensation for decoupled PIP joints (actuators are grounded):
-    // In the URDF, fingers are modeled as 2RR (MCP followed by PIP).
-    // The physical linkage means pip_joint_angle = pip_motor_angle - mcp_motor_angle.
-    // We apply this correction to the reported states for joints 3, 5, and 7.
-
-    // Index finger (PIP joint 3, MCP joint 2)
-    joint_position_states[3] -= joint_position_states[2];
-
-
-    // Middle finger (PIP joint 5, MCP joint 4)
-    joint_position_states[5] -= joint_position_states[4];
-
-
-    // Ring/Pinky finger (PIP joint 7, MCP joint 6)
-    joint_position_states[7] -= joint_position_states[6];
-
-    
-
-    counter_++;
+    // Apply kinematic compensation for PIP joints.
+    // The URDF models PIP joints relative to their MCP link,
+    // but the hardware reports absolute motor angles relative to the palm.
+    // Conversion: pip_joint_relative = pip_motor_absolute - mcp_motor_absolute
+    joint_position_states[3] -= joint_position_states[2];  // Index finger
+    joint_position_states[5] -= joint_position_states[4];  // Middle finger
+    joint_position_states[7] -= joint_position_states[6];  // Ring/Pinky finger
 }
 
 
 ////////////////////////////////////////////////////////////////////////
 
-void Hand::update_ft_sensor_states(std::vector<geometry_msgs::msg::Wrench> &ft_sensor_states){
-    // receive the message from the CAN bus every loop
-
+void Hand::update_ft_sensor_states(std::vector<geometry_msgs::msg::Wrench> &ft_sensor_states) {
     pcan_interface_.receive_message();
 
     for (size_t i = 0; i < ft_sensors_.size(); ++i) {
-        auto states = ft_sensors_[i].get_states();
+        const auto& states = ft_sensors_[i].get_states();
         ft_sensor_states[i].force.x = states.force_filtered.x();
         ft_sensor_states[i].force.y = states.force_filtered.y();
         ft_sensor_states[i].force.z = states.force_filtered.z();
@@ -212,73 +170,31 @@ void Hand::update_ft_sensor_states(std::vector<geometry_msgs::msg::Wrench> &ft_s
         ft_sensor_states[i].torque.y = states.torque_filtered.y();
         ft_sensor_states[i].torque.z = states.torque_filtered.z();
     }
-
-    // update the FT sensor states
-    
-    
 }
 
 
-////////////////////////////////////////////////////////////////////////
-
-void Hand::print_motor_positions(){
-    // useful function for offset calibration
-    std::cout << "J1 Motor Position: " << actuators_[0].get_motor_position() << std::endl;
-    std::cout << "J2 Motor Position: " << actuators_[1].get_motor_position() << std::endl;
-    std::cout << "J3 Motor Position: " << actuators_[2].get_motor_position() << std::endl;
-    std::cout << "J4 Motor Position: " << actuators_[3].get_motor_position() << std::endl;
-    std::cout << "J5 Motor Position: " << actuators_[4].get_motor_position() << std::endl;
-    std::cout << "J6 Motor Position: " << actuators_[5].get_motor_position() << std::endl;
-    std::cout << "J7 Motor Position: " << actuators_[6].get_motor_position() << std::endl;
-    std::cout << "J8 Motor Position: " << actuators_[7].get_motor_position() << std::endl;
-
-}
-
-
-////////////////////////////////////////////////////////////////////////
-
-void Hand::sort_can_rx_id_(const TPCANMsg &msg){
-      // Find the actuator corresponding to the received CAN Rx ID
-    auto it = actuator_rx_id_map_.find(msg.ID);
-
-    // Check if the key (CAN Rx ID) was found in the map
-    if (it != actuator_rx_id_map_.end()) {
-        // `it->second` is the Actuator* corresponding to the found CAN Rx ID
-        it->second->process_message(msg);
-    }
-}
- 
 ////////////////////////////////////////////////////////////////////////
 
 void Hand::print_actuator_info_() {  
     std::cout << "================== Actuators Info ===================" << std::endl;
+    std::cout << "[INFO] Total Actuators: " << actuators_.size() << std::endl;
 
-    // Total number of actuators
-    std::cout << "[INFO] Total Number of Actuators: " << actuators_.size() << " are initialized!" << std::endl;
-
-    // Actuator TX and RX IDs
     for (size_t i = 0; i < num_actuators_; ++i) {
-        std::cout << "[INFO] Actuator " << i + 1 
-                  << " TX ID: 0x" << std::hex << (int)actuators_[i].get_tx_id()
-                  << " RX ID: 0x" << std::hex << (int)actuators_[i].get_rx_id()
-
-                  << std::endl;
+        std::cout << "[INFO] Actuator " << (i + 1) 
+                  << " - TX: 0x" << std::hex << actuators_[i].get_tx_id()
+                  << ", RX: 0x" << actuators_[i].get_rx_id() << std::dec << std::endl;
     }
     
-    std::cout << "===================FT Sensors Info ==================" << std::endl;
+    std::cout << "=================== FT Sensors Info ==================" << std::endl;
+    std::cout << "[INFO] Total FT Sensors: " << ft_sensors_.size() << std::endl;
 
-    // Total number of FT sensors
-    std::cout << "[INFO] Total Number of FT Sensors: " << ft_sensors_.size() << " are initialized!" << std::endl;
-
-    // FT Sensor force and torque RX IDs
     for (size_t i = 0; i < ft_sensors_.size(); ++i) {
-        std::cout << "[INFO] FT Sensor " << i + 1 << " Force RX ID: 0x" << std::hex << (int)ft_sensors_[i].get_force_rx_id()
-                  << " Torque RX ID: 0x" << std::hex << (int)ft_sensors_[i].get_torque_rx_id() << std::endl;
+        std::cout << "[INFO] FT Sensor " << (i + 1)
+                  << " - Force RX: 0x" << std::hex << ft_sensors_[i].get_force_rx_id()
+                  << ", Torque RX: 0x" << ft_sensors_[i].get_torque_rx_id() << std::dec << std::endl;
     }
 }
 
-
 ////////////////////////////////////////////////////////////////////////
-
 
 } // namespace plato2_hand
